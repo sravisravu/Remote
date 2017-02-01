b60c85620ff8f075d5ced644c1c85cb1660150f6
Sravanthi Mailavaram <SMailavaram@newgistics.com>
2017-02-01 15:50:41 -0600

    initial commit

--- UnitTestProject2/UnitTestProject2/CommonMethods.cs
+++ UnitTestProject2/UnitTestProject2/CommonMethods.cs
@@ -17,9 +17,9 @@ namespace CloudReplicationTests
     public class CommonMethods
     {
         public static List<string> list = new List<string>(new string[] { "AddressDim", "AuditDim", "FacilityDim", "FwdContainer", "FwdPackage", "FwdUSPSPackage", "FwdUSPSPackageEvent", "OrgDim", "PostalCodeDim", "TrackingEventDim", "USPSFacilityDim", "USPSFacilityServiceZip", "USPSPricingCategoryDim", "USPSRateCategoryDim", "FwdTracking", "FwdPackageEvent", "DateDim" });
-        public static string connStringdw = "Database=" + ConfigurationManager.AppSettings["sngsdwDBName"] + ";Data Source=" + ConfigurationManager.AppSettings["sDBServer"] + ";Integrated Security=SSPI;";
+        public static string connStringdw = "Database=" + ConfigurationManager.AppSettings["sngsdwDBName"] + ";Data Source=" + ConfigurationManager.AppSettings["sDBServer"] + ";Integrated Security=False";
         //public static string connStringdw1 = "Server = tcp:" + ConfigurationManager.AppSettings["dDBServer"] + ",1433;Initial Catalog = DWCloud; Persist Security Info=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Authentication=Active Directory Integrated";
-        public static string connStringdw1 = "Database=" + ConfigurationManager.AppSettings["dngsdwDBName"] + ";Data Source=" + ConfigurationManager.AppSettings["dDBServer"] + ";Integrated Security=SSPI;";
+        public static string connStringdw1 = "Database=" + ConfigurationManager.AppSettings["dngsdwDBName"] + ";Data Source=" + ConfigurationManager.AppSettings["dDBServer"] + ";Integrated Security=False";
         public string schema = "dbo";
         public static int j;
         public static string source = "";
